61e3fdbbd15abd3d12283969bd35449ce74a4026
Ronald Nino <ronaldnino@12.0.16.172.net-uno.net>
2023-11-05 19:54:55 -0400

    add origis

--- src/main/java/org/web/demo/controllers/HelloController.java
+++ src/main/java/org/web/demo/controllers/HelloController.java
@@ -1,11 +1,13 @@
 package org.web.demo.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/getData")
     public String getMessage(){
         return "Esto es una ejemplo";
