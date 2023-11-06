e933a16631b4ce92b3f86a948cedba8852fbfc66
Ronald Nino <ronaldnino@12.0.16.172.net-uno.net>
2023-11-05 20:00:19 -0400

    change02 origin

--- src/main/java/org/web/demo/controllers/HelloController.java
+++ src/main/java/org/web/demo/controllers/HelloController.java
@@ -1,13 +1,13 @@
 package org.web.demo.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @CrossOrigin(origins = "http://localhost:9000")
+    //@CrossOrigin(origins = "http://localhost:9000")
     @GetMapping("/getData")
     public String getMessage(){
         return "Esto es una ejemplo";
